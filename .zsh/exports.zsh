70a7baeab22f7d2a0ca9991f757d855d78b9ef23
Steven Harman <steveharman@gmail.com>
2010-10-01 17:09:42 -0400

    no tabs for gvim commit message. dumb!

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,8 +1,8 @@
 export PATH=$PATH:/usr/local/bin:/usr/local/mongodb/bin:/usr/local/git/bin:/opt/local/lib/mysql5/bin:/opt/local/lib/postgresql83/bin:/opt/local/bin:/opt/local/sbin:~/Library/bin:/usr/local/lib/jruby-1.3.1/bin
-export CDPATH=:~/projects
+export CDPATH=:~/code
 export RUBYOPT='rubygems -Itest'
 export EDITOR="vim"
-export VISUAL="mvim --remote-tab-wait"
+export VISUAL="mvim -f"
 export GEM_OPEN_EDITOR="mvim"
 export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Home'
 export TERM=xterm-color
