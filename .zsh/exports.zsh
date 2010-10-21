2b8a2da4edef67d8714d7b875b0936da82afaf62
Steven Harman <steveharman@gmail.com>
2010-10-20 23:32:34 -0400

    use better Vim on OS X
    the Vim that currently ships with OS X is old (7.2) and doesn't seem to
    work for editing git commit messages from zsh, but the one in the
    MacVim.app bundle does.

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,8 +1,8 @@
 export PATH=$PATH:/usr/local/bin:/usr/local/mongodb/bin:/usr/local/git/bin:/opt/local/lib/mysql5/bin:/opt/local/lib/postgresql83/bin:/opt/local/bin:/opt/local/sbin:~/Library/bin:/usr/local/lib/jruby-1.3.1/bin
 export CDPATH=:~/code
 export RUBYOPT='rubygems -Itest'
-export EDITOR="vim"
-export VISUAL="mvim -f"
+export EDITOR="/Applications/MacVim.app/Contents/MacOS/Vim"
+#export VISUAL="mvim -f"
 export GEM_OPEN_EDITOR="mvim"
 export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Home'
 export TERM=xterm-color
