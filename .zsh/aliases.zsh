3663a5699ae015166238726278ee3249c756000b
Steven Harman <steveharman@gmail.com>
2012-03-08 17:42:22 -0500

    Add aliases for starting/stopping MySQL

--- .zsh/aliases.zsh
+++ .zsh/aliases.zsh
@@ -2,14 +2,16 @@
 alias b='cd -'
 alias cls='clear'
 alias sr='screen -r'
-alias pg_start='pg_ctl -D ~/.pgdata -l ~/.pgdata/psql.log start'
-alias pg_stop='pg_ctl -D ~/.pgdata stop -s -m fast'
 alias restart='touch tmp/restart.txt'
 alias ajaxrdoc="rdoc --fmt ajax --exclude '.*generator.*' --exclude '.*test.*' --exclude '.*spec.*'"
 alias spnr='rspec spec_no_rails'
 alias devlog='tail -200 -f log/development.log'
 alias testlog='tail -200 -f log/test.log'
 alias gvim='mvim -p'
+alias mysql_start='mysql.server start'
+alias mysql_stop='mysql.server stop'
+alias pg_start='pg_ctl -D ~/.pgdata -l ~/.pgdata/psql.log start'
+alias pg_stop='pg_ctl -D ~/.pgdata stop -s -m fast'
 alias redis_start='redis-server /usr/local/etc/redis.conf'
 
 function gitdays {
