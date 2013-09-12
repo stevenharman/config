482652ae28d557d4ae1ebafdc9e2e13c8cade4f5
Steven Harman <steveharman@gmail.com>
2013-09-12 12:23:40 -0400

    Tune Ruby GC

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,5 +1,7 @@
 export PATH="/usr/local/bin:/usr/local/sbin:$HOME/Library/bin:$PATH"
 export RBXOPT=-X19
+export RUBY_GC_MALLOC_LIMIT=90000000
+export RUBY_FREE_MIN=200000
 export CDPATH=:~/code
 export EDITOR="vim"
 #export VISUAL="mvim -f"
@@ -8,4 +10,3 @@ export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Home'
 export CLICOLOR=1
 export PGOPTIONS='-c client_min_messages=WARNING'
 export NODE_PATH="/usr/local/share/npm/bin"
-
