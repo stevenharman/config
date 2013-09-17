849dc11cac36d5a99f1c7468b33c58636ba36572
Steven Harman <steven@harmanly.com>
2013-09-16 20:27:51 -0400

    Organize ENV Var exports

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -1,14 +1,13 @@
-export PATH="/usr/local/bin:/usr/local/sbin:$HOME/Library/bin:$PATH"
-export RBXOPT=-X19
-export RUBY_GC_MALLOC_LIMIT=90000000
-export RUBY_FREE_MIN=200000
 export CDPATH=:~/code
+export CLICOLOR=1
 export EDITOR="vim"
-#export VISUAL="mvim -f"
 export GEM_OPEN_EDITOR="vim"
 export GOPATH="$HOME/code/go"
-export PATH="$PATH:$GOPATH/bin"
 export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Home'
-export CLICOLOR=1
-export PGOPTIONS='-c client_min_messages=WARNING'
 export NODE_PATH="/usr/local/share/npm/bin"
+export PATH="/usr/local/bin:/usr/local/sbin:$HOME/Library/bin:$GOPATH/bin:$PATH"
+export PGOPTIONS='-c client_min_messages=WARNING'
+export RBXOPT=-X19
+export RUBY_FREE_MIN=200000
+export RUBY_GC_MALLOC_LIMIT=90000000
+#export VISUAL="mvim -f"
