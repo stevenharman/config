881110b5e2b9a17d7241eb1b9147b31386261e49
Steven Harman <steven@harmanly.com>
2014-01-31 16:08:46 -0500

    Add Node NPM binaries to $PATH

--- .zsh/exports.zsh
+++ .zsh/exports.zsh
@@ -6,7 +6,7 @@ export GEM_OPEN_EDITOR="vim"
 export GOPATH="$HOME/code/go"
 export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Home'
 export NODE_PATH="/usr/local/share/npm/bin"
-export PATH="/usr/local/bin:/usr/local/sbin:$HOME/Library/bin:$GOPATH/bin:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:$HOME/Library/bin:$GOPATH/bin:$NODE_PATH:$PATH"
 export PGOPTIONS='-c client_min_messages=WARNING'
 export RBXOPT=-X19
 export RUBY_FREE_MIN=200000
