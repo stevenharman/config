b2fda34ec64f7f9fdad5399d676ae73963223e14
Steven Harman <steven@harmanly.com>
2014-12-22 14:56:15 -0500

    Remove NODE_PATH, which is not needed
    NPM learned about `prefix`, and so the Homebrew-installed Node + NPM
    will use prefix to symlink binaries into /usr/local/bin.

--- zsh.symlink/exports.zsh
+++ zsh.symlink/exports.zsh
@@ -5,8 +5,7 @@ export EDITOR="vim"
 export GEM_OPEN_EDITOR="vim"
 export GOPATH="$HOME/code/go"
 export JAVA_HOME='/System/Library/Frameworks/JavaVM.framework/Home'
-export NODE_PATH="/usr/local/share/npm/bin"
-export PATH="/usr/local/bin:/usr/local/sbin:$HOME/Library/bin:$GOPATH/bin:$NODE_PATH:$PATH"
+export PATH="/usr/local/bin:/usr/local/sbin:$HOME/Library/bin:$GOPATH/bin:$PATH"
 export PGOPTIONS='-c client_min_messages=WARNING'
 export RBXOPT=-X19
 export RUBY_GC_MALLOC_LIMIT=100000000
