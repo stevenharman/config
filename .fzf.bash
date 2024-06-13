8b889eb954e455edc3c305dabae17a52636a171f
Steven Harman <steven@harmanly.com>
2024-06-13 15:28:16 -0400

    Remove custom fzf bin path logic
    This is now built into the ~/.fzf/bin script, so we don't need this
    hack.

--- .fzf.bash
+++ .fzf.bash
@@ -1,21 +1,7 @@
-fzf_bin_dir="${HOME}/.fzf" # On Intel machines Homebrew installs fzf to $HOME
-fzf_opt_dir="${fzf_bin_dir}"
-if [ "$(arch)" = "arm64" ]; then
-  # On Apple Silicone machines Homebrew installs fzf elsewhere
-  fzf_bin_dir="/opt/homebrew/bin/fzf"
-  fzf_opt_dir="/opt/homebrew/opt/fzf"
-fi
-
 # Setup fzf
 # ---------
-if [[ ! "$PATH" == *"${fzf_bin_dir}/bin"* ]]; then
-  export PATH="${PATH:+${PATH}:}${fzf_bin_dir}/bin"
+if [[ ! "$PATH" == *"${HOME}/.fzf/bin"* ]]; then
+  export PATH="${PATH:+${PATH}:}${HOME}/.fzf/bin"
 fi
 
-# Auto-completion
-# ---------------
-[[ $- == *i* ]] && source "${fzf_opt_dir}/shell/completion.bash" 2> /dev/null
-
-# Key bindings
-# ------------
-source "${fzf_opt_dir}/shell/key-bindings.bash"
+eval "$(fzf --bash)"
