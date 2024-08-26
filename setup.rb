35ee593378f67903c29be1d238a748e65a3aaa26
Steven Harman <steven@harmanly.com>
2024-08-26 11:04:38 -0400

    Extract Dotfile setup
    Because next we're going to do some other shit too.

--- setup.rb
+++ setup.rb
@@ -2,71 +2,100 @@ require "pathname"
 
 module Setup
   def self.install(linkables:, home_dir:, working_dir:, overwrite_all: ENV.fetch("OVERWRITE_DOTFILES", false))
-    home_dir = Pathname(home_dir)
-    working_dir = Pathname(working_dir)
-    backup_all = false
-    skip_all = false
-    retried = false
-
-    linkables.each do |linkable|
-      skip = false
-      overwrite = false
-      backup = false
-
-      target = home_dir.join(linkable)
-
-      if target.exist? || target.symlink?
-        unless skip_all || overwrite_all || backup_all
-          puts "File already exists: #{target}, what do you want to do? [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all"
-          case $stdin.gets.chomp
-          when "o" then overwrite = true
-          when "b" then backup = true
-          when "O" then overwrite_all = true
-          when "B" then backup_all = true
-          when "S" then skip_all = true
-          when "s" then skip = true
-          end
-        end
-
-        next if skip || skip_all
-        target.delete if overwrite || overwrite_all
-
-        if backup || backup_all
-          bak = Pathname("#{target}.backup")
-          target.rename(bak)
-        end
-      end
-
-      dotfile = working_dir.join(linkable)
-      begin
-        target.make_symlink(dotfile)
-        retried = false
-      rescue Errno::ENOENT
-        raise if retried
-        retried = true
-        dir = target.dirname
-        dir.mkpath
-        retry
-      end
-    end
+    Dotfiles.setup(
+      linkables: linkables,
+      home_dir: home_dir,
+      working_dir: working_dir, overwrite_all: overwrite_all
+    )
   end
 
   def self.uninstall(linkables:, home_dir:)
-    home_dir = Pathname(home_dir)
+    Dotfiles.remove(linkables: linkables, home_dir: home_dir)
+  end
 
-    linkables.each do |linkable|
-      target = home_dir.join(linkable)
+  class Dotfiles
+    def self.setup(linkables:, home_dir:, working_dir:, overwrite_all:)
+      dotfiles = new(home_dir: home_dir, working_dir: working_dir, overwrite_all: overwrite_all)
+      dotfiles.setup(linkables: linkables)
+    end
 
-      # Remove all symlinks created during installation
-      if target.symlink?
-        target.delete
-      end
+    def self.remove(linkables:, home_dir:)
+      dotfiles = new(home_dir: home_dir)
+      dotfiles.remove(linkables: linkables)
+    end
 
-      # Replace any backups made during installation
-      backup = Pathname("#{target}.backup")
-      if backup.exist?
-        backup.rename(target)
+    def initialize(home_dir:, working_dir: Pathname.pwd, overwrite_all: false)
+      @home_dir = Pathname(home_dir)
+      @working_dir = Pathname(working_dir)
+      @overwrite_all = overwrite_all
+    end
+
+    def remove(linkables:)
+      linkables.each do |linkable|
+        target = home_dir.join(linkable)
+
+        # Remove all symlinks created during installation
+        if target.symlink?
+          target.delete
+        end
+
+        # Replace any backups made during installation
+        backup = Pathname("#{target}.backup")
+        if backup.exist?
+          backup.rename(target)
+        end
       end
     end
+
+    def setup(linkables:)
+      retried = false
+      backup_all = false
+      skip_all = false
+
+      linkables.each do |linkable|
+        skip = false
+        overwrite = false
+        backup = false
+
+        target = home_dir.join(linkable)
+
+        if target.exist? || target.symlink?
+          unless skip_all || overwrite_all || backup_all
+            puts "File already exists: #{target}, what do you want to do? [s]kip, [S]kip all, [o]verwrite, [O]verwrite all, [b]ackup, [B]ackup all"
+            case $stdin.gets.chomp
+            when "o" then overwrite = true
+            when "b" then backup = true
+            when "O" then overwrite_all = true
+            when "B" then backup_all = true
+            when "S" then skip_all = true
+            when "s" then skip = true
+            end
+          end
+
+          next if skip || skip_all
+          target.delete if overwrite || overwrite_all
+
+          if backup || backup_all
+            bak = Pathname("#{target}.backup")
+            target.rename(bak)
+          end
+        end
+
+        dotfile = working_dir.join(linkable)
+        begin
+          target.make_symlink(dotfile)
+          retried = false
+        rescue Errno::ENOENT
+          raise if retried
+          retried = true
+          dir = target.dirname
+          dir.mkpath
+          retry
+        end
+      end
+    end
+
+    attr_reader :home_dir, :working_dir
+    attr_accessor :overwrite_all
   end
 end
