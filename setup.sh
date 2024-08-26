08121958e8317facfe7bf45ecdf2eb74eb401b0d
Steven Harman <steven@harmanly.com>
2024-08-26 11:38:53 -0400

    Overwrite dotfiles when installing unattended

--- setup.sh
+++ setup.sh
@@ -1,3 +1,6 @@
 #!/usr/bin/env bash
 
-rake install
+# This file is used to automate setting up dotfiles in a CodeSpace/Coder.com,
+# meaning it's run without human intervertion. So, we need to overwrite
+# dotfiles to ensure we get the latest.
+OVERWRITE_DOTFILES=true rake install
